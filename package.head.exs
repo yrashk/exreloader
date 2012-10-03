0e4b074a6b7759e9ecf5ea6820e036ffab904751
Yurii Rashkovskii <yrashk@gmail.com>
2012-10-03 15:59:41 -0700

    Fix version in the expm package

--- package.head.exs
+++ package.head.exs
@@ -1,5 +1,5 @@
 Expm.Package.new(name: "exreloader", description: "Automatic module reloader",
-                 version: "0.0.1", keywords: ["module","otp","reload"], 
+                 version: :head, keywords: ["module","otp","reload"], 
                  maintainers: [[name: "Yurii Rashkovskii", 
                                 email: "yrashk@gmail.com"]],
                  repositories: [[github: "yrashk/exreloader"]])
